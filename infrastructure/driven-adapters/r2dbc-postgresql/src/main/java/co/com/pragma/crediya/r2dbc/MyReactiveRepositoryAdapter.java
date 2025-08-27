4c2afeb69884480ccce6c30dfe734af26998748e
JStivenpgSGM <jstivenpachong@gmail.com>
2025-08-26 22:03:41 -0500

    feat(MsAuthentication): The saveUser, deletedById and existsByEmail methods were created.

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/crediya/r2dbc/MyReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/crediya/r2dbc/MyReactiveRepositoryAdapter.java
@@ -1,23 +1,38 @@
 package co.com.pragma.crediya.r2dbc;
 
+import co.com.pragma.crediya.model.user.User;
+import co.com.pragma.crediya.model.user.gateways.UserRepository;
+import co.com.pragma.crediya.r2dbc.entity.UserEntity;
 import co.com.pragma.crediya.r2dbc.helper.ReactiveAdapterOperations;
+import co.com.pragma.crediya.r2dbc.mapper.UserEntityMapper;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public class MyReactiveRepositoryAdapter extends ReactiveAdapterOperations<
-    Object/* change for domain model */,
-    Object/* change for adapter model */,
-    String,
-    MyReactiveRepository
-> {
-    public MyReactiveRepositoryAdapter(MyReactiveRepository repository, ObjectMapper mapper) {
-        /**
-         *  Could be use mapper.mapBuilder if your domain model implement builder pattern
-         *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
-         *  Or using mapper.map with the class of the object model
-         */
-        super(repository, mapper, d -> mapper.map(d, Object.class/* change for domain model */));
+        User,
+        UserEntity,
+        Long,
+        MyReactiveRepository
+        > implements UserRepository {
+    private final UserEntityMapper userEntityMapper;
+    public MyReactiveRepositoryAdapter(MyReactiveRepository repository, ObjectMapper mapper, UserEntityMapper userEntityMapper) {
+        super(repository, mapper, d -> mapper.map(d, User.class));
+        this.userEntityMapper = userEntityMapper;
     }
-
+    @Override
+    public Mono<Void> deleteById(String id) {
+        return null;
+    }
+    @Override
+    public Mono<User> saveUser(User user) {
+        UserEntity toSave = userEntityMapper.toEntity(user);
+        return super.repository.save(toSave)
+                .map(userEntityMapper::toDomain);
+    }
+    @Override
+    public Mono<Boolean> existsByEmail(String email) {
+        return super.repository.existsByEmail(email);
+    }
 }
