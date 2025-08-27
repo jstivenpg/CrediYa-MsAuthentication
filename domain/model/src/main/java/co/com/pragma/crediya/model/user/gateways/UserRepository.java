fb873b96194748c5a7829bc29c1afd3f491a3132
JStivenpgSGM <jstivenpachong@gmail.com>
2025-08-26 22:14:08 -0500

    feat(MsAuthentication): The save and find methods were implemented.

--- domain/model/src/main/java/co/com/pragma/crediya/model/user/gateways/UserRepository.java
+++ domain/model/src/main/java/co/com/pragma/crediya/model/user/gateways/UserRepository.java
@@ -6,4 +6,6 @@ import reactor.core.publisher.Mono;
 public interface UserRepository {
   Mono<User> save(User user);
   Mono<Void> deleteById(String id);
+  Mono<User> saveUser(User user);
+  Mono<Boolean> existsByEmail(String email);
 }
