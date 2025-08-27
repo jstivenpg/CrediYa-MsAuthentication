91690b054a0103aa2d99d600ead3eb66a447531d
JStivenpgSGM <jstivenpachong@gmail.com>
2025-08-26 21:12:56 -0500

    feat(MsAuthentication): The save and deleteById methods were created.

--- domain/model/src/main/java/co/com/pragma/crediya/model/user/gateways/UserRepository.java
+++ domain/model/src/main/java/co/com/pragma/crediya/model/user/gateways/UserRepository.java
@@ -1,4 +1,9 @@
 package co.com.pragma.crediya.model.user.gateways;
 
+import co.com.pragma.crediya.model.user.User;
+import reactor.core.publisher.Mono;
+
 public interface UserRepository {
+  Mono<User> save(User user);
+  Mono<Void> deleteById(String id);
 }
