fb5bb8f9f02d42365ee6be2091b27baf8009fd62
JStivenpgSGM <jstivenpachong@gmail.com>
2025-08-27 21:57:35 -0500

    feat(MsAuthentication): SaveUser method was created.

--- domain/usecase/src/main/java/co/com/pragma/crediya/usecase/user/UserUseCase.java
+++ domain/usecase/src/main/java/co/com/pragma/crediya/usecase/user/UserUseCase.java
@@ -1,6 +1,22 @@
 package co.com.pragma.crediya.usecase.user;
 
+import co.com.pragma.crediya.model.user.User;
+import co.com.pragma.crediya.model.user.gateways.UserRepository;
+import co.com.pragma.crediya.usecase.user.exception.UserAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
 @RequiredArgsConstructor
 public class UserUseCase {
+    private final UserRepository userRepository;
+
+    public Mono<User> saveUser(User user) {
+        return userRepository.existsByEmail(user.getemail().email())
+                .flatMap(exists ->
+                        exists
+                                ? Mono.error(new UserAlreadyExistsException(user.getemail().email()))
+                                : userRepository.saveUser(user)
+                );
+
+    }
 }
