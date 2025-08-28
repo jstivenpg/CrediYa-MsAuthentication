a897d19744829298b230206b9b80df90838cac97
JStivenpgSGM <jstivenpachong@gmail.com>
2025-08-27 20:17:55 -0500

    feat(MsAuthentication): The save and findById methods were eliminated.

--- domain/model/src/main/java/co/com/pragma/crediya/model/user/gateways/UserRepository.java
+++ domain/model/src/main/java/co/com/pragma/crediya/model/user/gateways/UserRepository.java
@@ -4,8 +4,6 @@ import co.com.pragma.crediya.model.user.User;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository {
-  Mono<User> save(User user);
-  Mono<Void> deleteById(String id);
   Mono<User> saveUser(User user);
   Mono<Boolean> existsByEmail(String email);
 }
